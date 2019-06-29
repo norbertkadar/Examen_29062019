a570e385fa58fcc44995ff54b0073c7f09d91c1f
norbertkadar <norbert.kadar.a@gmail.com>
2019-06-29 10:45:56 +0300

    init repo examen

---
+++ ExamenAppDotNet/Models/ExamDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExamenAppDotNet.Models
+{
+    // DbContext = Unit of Work
+    public class ExamDbContext : DbContext
+    {
+        public ExamDbContext(DbContextOptions<ExamDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>(entity =>
+            {
+                entity.HasIndex(u => u.Username).IsUnique();
+            });
+        }
+
+        // DbSet = Repository, O tabela din baza de 
+        public DbSet<User> Users { get; set; }
+    }
+}
